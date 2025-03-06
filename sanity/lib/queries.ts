cdbc179f2cecab71e584ee0037766273e1e7a0e7
Liam Viray <neshiviray@gmail.com>
2025-03-07 02:57:12 +0800

    Updated projects query

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,9 @@
 import { defineQuery } from "next-sanity";
 
 export const PROJECTS_QUERY = defineQuery(
-  `*[_type == 'project' && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  `*[_type == 'project' && defined(slug.current) &&
+  (!defined($search) || $search == "" || title match $search || category match $search || author->name match $search)]
+  | order(_createdAt desc){
   _id,
   title,
   slug,
